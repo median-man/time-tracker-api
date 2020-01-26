d572bd7f480b0812bb8088230b03b21a0e77e472
John Desrosiers <john.desro@gmail.com>
2020-01-26 09:49:06 -0800

    Users routes fetches data using model

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const User = require("../models/User");
 
-const users = {
-  1: {
-    punches: [
-      {
-        type: "in",
-        timeStamp: Date.parse('2020-01-25T13:09:00')
-      },
-      {
-        type: "out",
-        timeStamp: Date.parse('2020-01-25T14:09:00')
-      }
-    ]
-  }
-}
+const router = express.Router();
 
-/* GET home page. */
-router.get('/:id', function(req, res, next) {
-  res.json(users[parseInt(req.params.id)]);
+router.get("/", async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+// get user by the user id
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.json(user);
 });
 
 module.exports = router;
