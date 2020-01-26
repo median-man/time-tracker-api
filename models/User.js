d43c6b9f8fa7e6c217036efdb2d5cb8805aa2bfe
John Desrosiers <john.desro@gmail.com>
2020-01-26 10:24:15 -0800

    Add static method to create punches

--- models/User.js
+++ models/User.js
@@ -1,20 +1,25 @@
 const mongoose = require("mongoose");
 const { Schema, SchemaTypes } = mongoose;
 
+const punchSchema = new Schema({
+  type: {
+    type: String,
+    enum: ["in", "out"],
+    required: true
+  },
+  timeStamp: {
+    type: Number,
+    required: true
+  }
+})
+
 const userSchema = new Schema({
-  punches: [
-    {
-      type: {
-        type: String,
-        enum: ["in", "out"],
-        required: true
-      },
-      timeStamp: {
-        type: Number,
-        required: true
-      }
-    }
-  ]
+  punches: [punchSchema]
 });
+
+userSchema.static("createPunch", function(userId, newPunch, options) {
+  console.log(userId, newPunch, options)
+  return this.findByIdAndUpdate(userId, { $push: { punches: newPunch }}, options)
+})
 
 module.exports = mongoose.model("User", userSchema);
