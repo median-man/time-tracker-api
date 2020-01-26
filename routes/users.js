27cfd5ba2d24ad04bd2f6b5e973fe7401cb75b52
John Desrosiers <john.desro@gmail.com>
2020-01-26 10:24:35 -0800

    Add POST /users/:id/punches route

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const debug = require("debug")("time-tracker-api:server");
 const User = require("../models/User");
 
 const router = express.Router();
@@ -12,6 +13,25 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const user = await User.findById(req.params.id);
   res.json(user);
+});
+
+// create a new punch for user
+router.post("/:id/punches", async (req, res) => {
+  try {
+    await User.createPunch(req.params.id, {
+      type: req.body.type,
+      timeStamp: req.body.timeStamp
+    });
+    res.send("punch added");
+  } catch (error) {
+    if (error.name && error.name === "CastError") {
+      debug("Error trying to create punch. %s", error.message);
+      res.status(400);
+    } else {
+      res.status(500);
+    }
+    res.end();
+  }
 });
 
 module.exports = router;
