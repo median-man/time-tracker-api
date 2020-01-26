be2ba0544cc749a02d45a3059e3aede0b9dc1967
John Desrosiers <john.desro@gmail.com>
2020-01-26 09:35:08 -0800

    Configure db connection

---
+++ db/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const debug = require("debug")("time-tracker-api:db");
+
+const DISCONNECTED = 0;
+
+module.exports.connect = () => {
+  if (mongoose.connection.readyState === DISCONNECTED) {
+    mongoose.connection.on("error", error => debug("Error: %O", error));
+    mongoose.connection.once("open", () => debug("connected to mongodb"));
+    return mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://localhost/timetracker",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    );
+  }
+  return mongoose;
+};
