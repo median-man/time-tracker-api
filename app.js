90c235e6b58e76bb5be4b72d4efff9cc39bda68f
John Desrosiers <john.desro@gmail.com>
2020-01-26 08:02:21 -0800

    Remove example routes from boilerplate

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+// const path = require('path');
+// TODO: uninstall cookieParser if not using cookies for session management
+// const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
+// TODO: use logger conditionally based on NODE_ENV
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// TODO: remove cookieParse if not using cookies...
+// app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
