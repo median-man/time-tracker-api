bebbf95ba968cc7be477d902a40aa47f500807c7
John Desrosiers <john.desro@gmail.com>
2020-01-26 08:32:08 -0800

    Add cors and helmet middleware

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 // const path = require('path');
 // TODO: uninstall cookieParser if not using cookies for session management
 // const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
+// const indexRouter = require('./routes/index');
+// const usersRouter = require("./routes/users");
 
 const app = express();
 
 // TODO: use logger conditionally based on NODE_ENV
-app.use(logger('dev'));
+app.use(logger("dev"));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// TODO: remove cookieParse if not using cookies...
+// TODO: remove cookieParser if not using cookies...
 // app.use(cookieParser());
 
-app.use('/', indexRouter);
+// TODO: limit allowed urls to a whitelist
+app.use(cors());
+
+// app.use('/', indexRouter);
+// app.use("/users", usersRouter);
 
 module.exports = app;
